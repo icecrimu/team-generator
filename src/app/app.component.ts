8cd97afec0dd06139a2eee9c9d80200d7579721a
icecrimu <charles.banigon@gmail.com>
2022-10-02 11:10:23 +0800

    Error Handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,20 @@ export class AppComponent {
   title = 'team-generator';
   newMemberName = '';
   members: string[] = [];
+  errorMessage = '';
 
   onInput(member: string) {
     this.newMemberName = member;
   }
 
   addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty!";
+      return;
+    }
+
+    this.errorMessage = '';
+
     this.members.push(this.newMemberName);
     this.newMemberName = '';
     console.log(this.members);
