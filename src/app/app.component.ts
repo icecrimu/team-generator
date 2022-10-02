9c5d2f3ca2dd06d16dccbdc4cb833472ad9a5f03
icecrimu <charles.banigon@gmail.com>
2022-10-02 12:05:55 +0800

    Team Logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,15 @@ export class AppComponent {
   newMemberName = '';
   members: string[] = [];
   errorMessage = '';
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
 
   onInput(member: string) {
     this.newMemberName = member;
+  }
+
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
   }
 
   addMember() {
@@ -26,5 +32,27 @@ export class AppComponent {
     this.members.push(this.newMemberName);
     this.newMemberName = '';
     console.log(this.members);
+  }
+
+  generateTeams() {
+    const allMembers = [...this.members];
+
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (member == undefined) break;
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+
+    console.log(this.teams);
   }
 }
